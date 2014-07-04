452b0492a190cbb0f3d532beaac30ea29dd87fea
hul <hl3w22bupt@gmail.com>
2014-07-05 02:39:59 +0800

    update src

--- coroutine/mc_coroutine.h
+++ coroutine/mc_coroutine.h
@@ -4,12 +4,20 @@
 #include <ucontext.h>
 #include <tr1/unordered_map>
 
-namespace MagicCube {
+namespace magic_cube {
+
+enum CoroutineState {
+    COROUTINE_READY   = 0,
+    COROUTINE_RUNNING = 1,
+    COROUTINE_SUSPEND = 2,
+    COROUTINE_DONE    = 3,
+};
 
 typedef int (*coroutine_func)(void*);
 
 typedef struct CoroutineCtx {
-    ucontext_t ctx;       // 当前协程上下文
+    ucontext_t uctx;      // 当前协程上下文
+    int idx;              // 当前协程idx
     int state;            // 当前协程状态
     int yield;            // 传递给其它ctx的参数
     coroutine_func func;  // 协程回调函数
@@ -20,14 +28,18 @@ typedef std::tr1::unordered_map<int, COROUTINECTX*> CoroutineTable;
 
 class CoroutineScheduler {
     public:
+        CoroutineScheduler();
+        CoroutineScheduler(int cocurrent_num, int stack_size);
+        ~CoroutineScheduler();
 
+    public:
         /// @brief  创建协程实例
         ///
         /// @return
         int CreateCoroutine();
 
         /// @brief  销毁协程实例
-        void DestroyCoroutine();
+        void DestroyCoroutine(int uct_id);
 
         /// @brief  协程让出CPU占用权
         ///
@@ -43,14 +55,20 @@ class CoroutineScheduler {
         /// @return
         int ResumeCoroutine(int uct_id);
 
-    private:
-        int running_cid_;
-        ucontext_t main_ctx_;
+        bool CoroutineBeenAlive(int uct_id);
 
-        size_t stack_size_;
-        int coroutine_max_;
-        struct CoroutineCtx* coroutine_list_;
-        CoroutineTable coroutine_table_;
+        // noncopyable
+    private:
+        CoroutineScheduler(const CoroutineScheduler&);
+        CoroutineScheduler& operator=(const CoroutineScheduler&);
+
+    private:
+        int running_cid_;                       // 当前运行coroutine id
+        ucontext_t main_ctx_;                   // 主coroutine
+
+        size_t stack_size_;                     // coroutine stack大小
+        int coroutine_max_;                     // 最大同时支持协程数
+        CoroutineTable coroutine_table_;        // 协程表
 };
 
 }
