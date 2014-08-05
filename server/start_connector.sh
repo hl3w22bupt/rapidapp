6f93d21ee18796b60b193c444b23a79447efbf1c
hul <hl3w22bupt@gmail.com>
2014-08-05 17:38:04 +0800

    add scripts

---
+++ server/start_connector.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+./connector_svr --uri=tcp:127.0.0.1:8888 --minloglevel=1 &
---
+++ tools/start_echosvr.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+./echo_svr --uri=tcp:127.0.0.1:9091 --minloglevel=1 &
+./echo_svr --uri=tcp:127.0.0.1:9092 --minloglevel=1 &
