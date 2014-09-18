74514623ba8d35adee11d05450654e9600d3d702
hul <hl3w22bupt@gmail.com>
2014-09-18 21:28:29 +0800

    update add commnet

--- server/connector/client_api/connector_client_api.cpp
+++ server/connector/client_api/connector_client_api.cpp
@@ -1,4 +1,5 @@
 #include "connector_client_api.h"
+#include "../client.pb.h"
 
 namespace hmoon_connector_api {
 
@@ -8,24 +9,29 @@ ConnectorClientApi::ConnectorClientApi()
 ConnectorClientApi::~ConnectorClientApi()
 {}
 
+// 设置一些参数，比如登录帐号、token、密码等
 int ConnectorClientApi::Init()
 {
     return 0;
 }
 
+// 释放资源
 void ConnectorClientApi::CleanUp()
 {}
 
+// 发起服务连接
 int ConnectorClientApi::Start()
 {
     return 0;
 }
 
+// 停止服务连接
 int ConnectorClientApi::Stop()
 {
     return 0;
 }
 
+// 恢复服务连接
 int ConnectorClientApi::Resume()
 {
     return 0;
