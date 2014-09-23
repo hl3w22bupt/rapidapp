c5f1df4c3cc383ff323aabc8d4d58bf1eb702f39
hul <hl3w22bupt@gmail.com>
2014-09-23 20:11:48 +0800

    update script

--- codecount.sh
+++ codecount.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-find ./ -path "./thirdparty/*/*" -prune -o -type f -name "*.cpp" | xargs wc -l
-find ./ -path "./thirdparty/*/*" -prune -o -type f -name "*.h" | xargs wc -l
+find ./ -path "./thirdparty" -prune -o -type f -name "*.cpp" | xargs wc -l
+find ./ -path "./thirdparty" -prune -o -type f -name "*.h" | grep -v ".pb.h" | xargs wc -l
