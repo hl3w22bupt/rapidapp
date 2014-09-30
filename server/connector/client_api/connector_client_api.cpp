93a52ce8ff30a2401f9b75cc774edb64684194f0
hul <hl3w22bupt@gmail.com>
2014-09-30 20:50:47 +0800

    update client api

--- server/connector/client_api/connector_client_api.cpp
+++ server/connector/client_api/connector_client_api.cpp
@@ -6,13 +6,19 @@
 
 namespace hmoon_connector_api {
 
-ConnectorClientProtocol::ConnectorClientProtocol()
+ConnectorClientProtocol::ConnectorClientProtocol() :
+    protocol_event_listener_(NULL)
 {}
 
-//ConnectorClientProtocol::~ConnectorClientProtocol()
-//{}
+ConnectorClientProtocol::~ConnectorClientProtocol()
+{}
 
-int ConnectorClientProtocol::Run(IProtocolEventListener* protocol_evlistener)
+int ConnectorClientProtocol::Connect()
+{
+    return 0;
+}
+
+int ConnectorClientProtocol::Start(IProtocolEventListener* protocol_evlistener)
 {
     if (NULL == protocol_evlistener)
     {
@@ -21,20 +27,15 @@ int ConnectorClientProtocol::Run(IProtocolEventListener* protocol_evlistener)
     protocol_event_listener_ = protocol_evlistener;
 
     // 设置一些参数，比如登录帐号、token、密码等
+    protocol_event_listener_->OnGetSettings(appid_, openid_, token_,
+                                            encrypt_mode_, auth_type_,
+                                            server_ip_, server_port_);
     // 发起服务连接
-    return 0;
-}
-
-int ConnectorClientProtocol::RunWithThread(IProtocolEventListener* protocol_evlistener)
-{
-    if (NULL == protocol_evlistener)
+    int ret = Connect();
+    if (ret != 0)
     {
         return -1;
     }
-
-    boost::thread protocol_thread(boost::bind(&ConnectorClientProtocol::Run,
-                                              this, protocol_evlistener));
-    protocol_thread.detach();
 
     return 0;
 }
@@ -47,6 +48,70 @@ int ConnectorClientProtocol::Terminate()
 
 // 恢复服务
 int ConnectorClientProtocol::Resume()
+{
+    return 0;
+}
+
+// 驱动异步事件
+int ConnectorClientProtocol::Update()
+{
+    return 0;
+}
+
+// 发送消息
+int ConnectorClientProtocol::PushMessage()
+{
+    return 0;
+}
+
+// 接收消息
+int ConnectorClientProtocol::PopMessage()
+{
+    return 0;
+}
+
+///////////////////////////////////////////
+//////////////  Thread ////////////////////
+///////////////////////////////////////////
+ConnectorClientProtocolThread::ConnectorClientProtocolThread() : ccproto_(NULL), exit_(false)
+{}
+
+ConnectorClientProtocolThread::~ConnectorClientProtocolThread()
+{}
+
+int ConnectorClientProtocolThread::MainLoop(IProtocolEventListener* protocol_evlistener)
+{
+    // 1. Start Connection
+    // 2. Update Connection
+    return 0;
+}
+
+int ConnectorClientProtocolThread::RunWithThread(IProtocolEventListener* protocol_evlistener)
+{
+    if (NULL == protocol_evlistener)
+    {
+        return -1;
+    }
+
+    ccproto_ = ConnectorClientProtocol::Create();
+    boost::thread protocol_thread(boost::bind(&ConnectorClientProtocolThread::MainLoop,
+                                              this, protocol_evlistener));
+    protocol_thread.detach();
+
+    return 0;
+}
+
+int ConnectorClientProtocolThread::TerminateThread()
+{
+    return 0;
+}
+
+int ConnectorClientProtocolThread::PushMessageToSendQ()
+{
+    return 0;
+}
+
+int ConnectorClientProtocolThread::PopMessageFromRecvQ()
 {
     return 0;
 }
--- server/connector/client_api/connector_client_api.h
+++ server/connector/client_api/connector_client_api.h
@@ -1,7 +1,20 @@
 #ifndef _CONNECTOR_CLIENT_API_H_
 #define _CONNECTOR_CLIENT_API_H_
 
+#include <string>
+
 namespace hmoon_connector_api {
+
+enum {
+    NOT_ENCRYPT = 0,  // 不加密 - 适用于早期测试阶段
+    ENCRYPT_AES = 1,  // AES对称加密
+    ENCRYPT_RSA = 2,  // RSA非对称加密
+};
+
+enum {
+    NONE_AUTHENTICATION = 0,    // 非鉴权 - 适用于早期测试阶段和游客登录模式
+    AUTHENTICATION_ATK  = 1,
+};
 
 class IProtocolEventListener {
     public:
@@ -9,8 +22,10 @@ class IProtocolEventListener {
         virtual ~IProtocolEventListener(){};
 
     public:
-        virtual int Init() = 0;
-        virtual void CleanUp() = 0;
+        // 为了方便获取多个参数值，同时为了编程方便，采用引用的方式
+        virtual void OnGetSettings(std::string& appid, std::string& openid,
+                                   std::string& token, int& encrypt_mode, int& auth_type,
+                                   std::string& server_ip, int& server_port) = 0;
 
         virtual int OnHandShakeSucceed() = 0;
         virtual int OnHandShakeFailed() = 0;
@@ -31,19 +46,69 @@ class ConnectorClientProtocol {
         }
 
     public:
-        int Run(IProtocolEventListener* protocol_evlistener);
-        int RunWithThread(IProtocolEventListener* protocol_evlistener);
-
-        int Resume();
+        int Start(IProtocolEventListener* pel);
         int Terminate();
+        int Resume();
+
+        int Update();
+
+        int PushMessage();
+        int PopMessage();
+
+    // 为了编程方便，同时考虑到短期类成员变量变化不频繁，
+    // 所有类的非public成员都不采用pimpl实现
+    private:
+        int Connect();
 
     private:
         ConnectorClientProtocol();
+        ~ConnectorClientProtocol();
         ConnectorClientProtocol(const ConnectorClientProtocol&);
         ConnectorClientProtocol& operator=(const ConnectorClientProtocol&);
 
     private:
         IProtocolEventListener* protocol_event_listener_;
+
+    private:
+        std::string appid_;
+        std::string openid_;
+        std::string token_;
+        int encrypt_mode_;
+        int auth_type_;
+        std::string server_ip_;
+        int server_port_;
+};
+
+class ConnectorClientProtocolThread {
+    public:
+        static ConnectorClientProtocolThread& Singleton() {
+            static ConnectorClientProtocolThread instance;
+            return instance;
+        }
+
+        static ConnectorClientProtocolThread* Create() {
+            return new ConnectorClientProtocolThread;
+        }
+
+    public:
+        int RunWithThread(IProtocolEventListener* protocol_evlistener);
+        int TerminateThread();
+
+        int PushMessageToSendQ();
+        int PopMessageFromRecvQ();
+
+    private:
+        int MainLoop(IProtocolEventListener* protocol_evlistener);
+
+    private:
+        ConnectorClientProtocolThread();
+        ~ConnectorClientProtocolThread();
+        ConnectorClientProtocolThread(const ConnectorClientProtocolThread&);
+        ConnectorClientProtocolThread& operator=(const ConnectorClientProtocol&);
+
+    private:
+        ConnectorClientProtocol* ccproto_;
+        bool exit_;
 };
 
 }
